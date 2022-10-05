837f503e0e0fc39005c290d94e95c35e95d342a7
shamimspro <shamimspro@gmail.com>
2022-10-05 16:24:20 +0600

    nav issue

--- src/components/Region/Region.jsx
+++ src/components/Region/Region.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Region = ({img_region, name_region}) => {
     return (
-        <div>
-            <img src={img_region} alt="region_image" />
-            <h2>{name_region}</h2>
+        <div className='border'>
+            <img className='w-full' src={img_region} alt="region_image" />
+            <h2 className='text-2xl font-bold text-center'>{name_region}</h2>
         </div>
     );
 };
